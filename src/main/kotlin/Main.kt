881fb975450013bc2af25fc0157a7717bf460b37
Ланин Сергей Дмитриевич <10221261@college.local>
2023-09-05 20:49:15 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+//fun main() {
+//    val a = readln().groupingBy { it }.eachCount()
+//    for (item in a) {
+//        print(item.toString().replace("[=1]".toRegex(), ""))
+//    }
+
+
+//}//fun main() {
+//    val a = readln().groupingBy { it }.eachCount().toSortedMap()
+//    for (item in a)
+//        println(item.toString().replace('=', '-'))
+//}
+
+
+
+
+//fun main() {
+//    print(Integer.toBinaryString(Integer.parseInt(readln())))
+//}
+
+//fun main() {
+//    val firstNumber: Double = readln()
+//    val secondNumber: Double = readln()
+//
+//
+//        val o = readLine().toString()[0]
+//        var result: Double? = null
+//
+//        if (o == '+') {
+//            result = firstNumber + secondNumber
+//        } else if (o == '-') {
+//            result = firstNumber - secondNumber
+//        } else if (o == '*') {
+//            result = firstNumber * secondNumber
+//        } else if (o == '/') {
+//            result = firstNumber / secondNumber
+//        } else {
+//            println("Please enter a valid operator.")
+//        }
+//
+//        if (result != null) {
+//            println("$firstNumber $o $secondNumber = $result")
+//        }
+//}
