48d73fc42f0a77ccee8161f9de2c34948ca12ac7
Sergey <95441430+dangen-master@users.noreply.github.com>
2023-09-07 19:02:48 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,24 @@
 //fun main() {
-//    val a = readln().groupingBy { it }.eachCount()
-//    for (item in a) {
-//        print(item.toString().replace("[=1]".toRegex(), ""))
+//    val a: String = readln()
+//    var b: Int = 1
+//    var c: String = ""
+//    for (i: Int in 1 until a.length) {
+//        if (a[i - 1] == a[i]) {
+//            b += 1
+//        } else {
+//            c += a[i - 1]
+//            if (b > 1) {
+//                c += b
+//            }
+//            b = 1
+//        }
+//        if (i == a.length - 1) {
+//            c += a[i]
+//            if (b > 1) c += b
+//        }
 //    }
+//    println(c)
+//}
 
 
 //}//fun main() {
@@ -12,33 +28,59 @@
 //}
 
 
-
-
 //fun main() {
 //    print(Integer.toBinaryString(Integer.parseInt(readln())))
 //}
 
 //fun main() {
-//    val firstNumber: Double = readln()
-//    val secondNumber: Double = readln()
+//    println("Укажите два числа и символ операции, пример: 2 2 +. Допустимые символы операции: (+/-*) ")
+//    val data: String = readln()
+//    val array: List<String> = data.split(' ')
+//
+//    var operator: String = array[2]
+//    var operand1: Double = array[0].toDouble()
+//    var operand2: Double = array[1].toDouble()
+//    var res: Double? = null;
+//
+//    when (operator) {
+//        "+" -> res = operand1 + operand2
+//        "-" -> res = operand1 - operand2
+//        "*" -> res = operand1 * operand2
+//        "/" -> res = operand1 / operand2
+//    }
+//
+//    println("Результат: $res")
+//}
+
+//fun main() {
+//    println("Введите целое число n и основание степени x...")
+//
+//    var n: Double = readln().toDouble()
+//    var x: Double = readln().toDouble()
+//    var res = ln(n) / ln(x);
 //
 //
-//        val o = readLine().toString()[0]
-//        var result: Double? = null
+//    if (res == floor(res)) {
+//        println("целочисленный показатель = $res")
+//    } else {
+//        println("Целочисленный показатель не существует")
+//    }
+//}
+
+//fun main() {
+//    println("Укажите первую цифру")
 //
-//        if (o == '+') {
-//            result = firstNumber + secondNumber
-//        } else if (o == '-') {
-//            result = firstNumber - secondNumber
-//        } else if (o == '*') {
-//            result = firstNumber * secondNumber
-//        } else if (o == '/') {
-//            result = firstNumber / secondNumber
-//        } else {
-//            println("Please enter a valid operator.")
-//        }
+//    var a: Int = readln().toInt()
 //
-//        if (result != null) {
-//            println("$firstNumber $o $secondNumber = $result")
-//        }
+//    println("Укажите вторую цифру")
+//
+//    var b: Int = readln().toInt()
+//
+//    if ((a.toString() + b.toString()).toInt() % 2 == 1) {
+//        println("Ваше нечетное число = $a$b")
+//    } else if ((b.toString() + a.toString()).toInt() % 2 == 1) {
+//        println("Ваше нечетное число = $b$a")
+//    } else {
+//        println("Создать нечетное число невозможно")
+//    }
 //}
